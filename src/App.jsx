e9ef22254068f09dda8086110eda4b3c294d7182
matheusSantkk <matheusantana731@gmail.com>
2026-04-08 00:19:02 -0300

    chore: scaffold project structure and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import './App.css';
 
 const App = () => {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
