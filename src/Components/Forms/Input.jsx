a02bcc6740c87ebee9643e487f7d44d79a5770f7
matheusSantkk <matheusantana731@gmail.com>
2026-04-13 14:56:03 -0300

    feat: add custom useForm hook with regex validation and blur support

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,13 +1,21 @@
 import styles from "../../Styles/Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      <input id={name} name={name} type={type} className={styles.input}></input>
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        className={styles.input}
+      ></input>
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
